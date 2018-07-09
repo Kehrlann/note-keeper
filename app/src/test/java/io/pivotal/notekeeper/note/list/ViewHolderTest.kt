1364bc1807f1df43342fb756d90dd8b1aac60a5a
Daniel Garnier-Moiroux <git@garnier.wf>
2018-07-09 23:56:26 +0200

    Test wiring of xxClickListener in the NoteViewHolder

--- app/src/test/java/io/pivotal/notekeeper/note/list/ViewHolderTest.kt
+++ app/src/test/java/io/pivotal/notekeeper/note/list/ViewHolderTest.kt
@@ -14,16 +14,27 @@ import org.robolectric.RuntimeEnvironment
 @RunWith(RobolectricTestRunner::class)
 class ViewHolderTest {
 
+    private val view = LayoutInflater
+            .from(RuntimeEnvironment.application.applicationContext)
+            .inflate(R.layout.note_card, null, false) as ConstraintLayout
+
     @Test
     fun `displays a title and preview`() {
-        val view = LayoutInflater
-                .from(RuntimeEnvironment.application.applicationContext)
-                .inflate(R.layout.note_card, null, false) as ConstraintLayout
-
         val viewHolder = ViewHolder(view, null)
         viewHolder.note = Note(12, "Title", "My Text")
 
         assertThat(viewHolder.view.findViewById<TextView>(R.id.card_title).text).isEqualTo("Title")
         assertThat(viewHolder.view.findViewById<TextView>(R.id.card_preview).text).isEqualTo("My Text")
+    }
+
+    @Test
+    fun `calls the xxClickListener when clicking on note`() {
+        var handlerWasCalledWith: Int? = null
+        val viewHolder = ViewHolder(view, { position -> handlerWasCalledWith = position })
+        viewHolder.note = Note(12, "", "")
+
+        viewHolder.onClick(view)
+
+        assertThat(handlerWasCalledWith).isEqualTo(12)
     }
 }
