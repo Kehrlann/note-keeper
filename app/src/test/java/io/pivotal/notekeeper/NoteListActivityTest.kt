f233f783126d9b81c512bc9d272c4c81b67c51c9
Daniel Garnier-Moiroux <git@garnier.wf>
2018-07-09 22:54:24 +0200

    Use fake app in NoteListActivity test
    - Now uses the fake so we can avoid relying on the
      actual implementation of the note service

--- app/src/test/java/io/pivotal/notekeeper/NoteListActivityTest.kt
+++ app/src/test/java/io/pivotal/notekeeper/NoteListActivityTest.kt
@@ -2,11 +2,13 @@ package io.pivotal.notekeeper
 
 import android.support.v7.widget.RecyclerView
 import io.pivotal.notekeeper.note.list.NoteListActivity
+import io.pivotal.notekeeper.testdoubles.FakeNoteKeeperApplication
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.Robolectric
 import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -14,12 +16,13 @@ import org.robolectric.RobolectricTestRunner
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(RobolectricTestRunner::class)
+@Config(application = FakeNoteKeeperApplication::class)
 class NoteListActivityTest {
     @Test
     fun `displays a list of cards`() {
         val mainActivity = Robolectric.setupActivity(NoteListActivity::class.java)
         val recyclerView: RecyclerView = mainActivity.findViewById(R.id.note_list)
         assertThat(recyclerView).isNotNull
-        assertThat(recyclerView.childCount).isGreaterThan(1)
+        assertThat(recyclerView.childCount).isEqualTo(3)
     }
 }
--- app/src/test/java/io/pivotal/notekeeper/testdoubles/FakeNoteKeeperApplication.kt
+++ app/src/test/java/io/pivotal/notekeeper/testdoubles/FakeNoteKeeperApplication.kt
@@ -9,5 +9,6 @@ import io.pivotal.notekeeper.note.NoteService
 class FakeNoteKeeperApplication: NoteKeeperApplication() {
     override var noteService = mock<NoteService> {
         on { note(42) } doReturn Note(42, "Life, the Universe and Everything", "So long and thanks for all the fish")
+        on { notes } doReturn (1..3).map { Note(it, "Life, volume $it", "So long and thanks for all the fish") }
     }
 }
