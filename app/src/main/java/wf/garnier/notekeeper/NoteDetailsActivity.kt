b6b84cc7f4fb4d26abc985fc91583a7b73f4370a
Daniel Garnier-Moiroux <git@garnier.wf>
2018-10-02 08:33:48 +0200

    Add (empty) NoteDetailsActivity

---
+++ app/src/main/java/wf/garnier/notekeeper/NoteDetailsActivity.kt
@@ -0,0 +1,12 @@
+package wf.garnier.notekeeper
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class NoteDetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_note_details)
+    }
+}
